6bb7c7f4a63f479c60d834259d061e7ddf025224
mixomatosys <75966190+mixomatosys@users.noreply.github.com>
2022-05-23 00:46:41 -0400

    Update import-gpos.ps1

--- powershell/import-gpos.ps1
+++ powershell/import-gpos.ps1
@@ -1,7 +1,7 @@
-$domain=Read-Host "Enter the domain to import and link T0 GPOs"
+$domain=Read-Host "Enter the domain to import and link GPOs"
 klist purge > $Null
 $DN=$(Get-addomain -server $domain).distinguishedname
-$path ='E:\Snapshots\GPOBackups\2019_02_22'
+$path ='[INSERT PATH TO OU]'
 
 # list of gpos backed up in folder
 $gpos=$(Get-ChildItem $path).name
@@ -14,15 +14,15 @@ foreach ($GPO in $gpos)
     {
         #link policy?
         write-host "$GPO Exists - Linking to ou=t0,ou=vzdas,$DN"
-        $GPOExists| New-GPLink -target "ou=t0,ou=vzdas,$DN"  -Domain $domain
-        #Get-GPO -Name $gpo -domain $domain | New-GPLink -target "ou=t0,ou=vzdas,$DN"  -Domain $domain
+        $GPOExists| New-GPLink -target "[INSERT OU],$DN"  -Domain $domain
+        #Get-GPO -Name $gpo -domain $domain | New-GPLink -target "[INSERT OU],$DN"  -Domain $domain
     }
     Else
     {
         write-host "$GPO did not exist - importing from $path\$gpo"
         # gpo didnt exist - create GPO and import from backup
         Import-GPO -CreateIfNeeded -BackupGpoName "$gpo" -TargetName "$gpo"  -Path "$path\$gpo"  -Domain $domain
-        write-host "Linking $gpo to ou=t0,ou=vzdas,$DN"
-        Get-GPO -Name "$gpo" -domain $domain | New-GPLink -target "ou=t0,ou=vzdas,$DN" -Domain $domain
+        write-host "Linking $gpo to [INSERTOU]$DN"
+        Get-GPO -Name "$gpo" -domain $domain | New-GPLink -target "[INSERT OU]$DN" -Domain $domain
      }
 }
